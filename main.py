1e6274a7b448a6bfb2bed8a2e823b9bf2e93348b
Lyubo <175617550+cryptsoul@users.noreply.github.com>
2026-04-11 12:26:45 +0300

    Minor UI update to main window

--- main.py
+++ main.py
@@ -1,38 +1,24 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QPushButton, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFont
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setGeometry(0, 0, self.width(), self.height())
         self.controls()
         self.initUI()
 
     def initUI(self):
-        central_widget = QWidget()
-        self.setCentralWidget(central_widget)
+        self.setGeometry(400, 200, 1000, 700)
+        self.setWindowTitle("TimeFlow")
+        self.setStyleSheet("background-color: #202D34;")
         
-        label1 = QLabel("#1", self)
-        label2 = QLabel("#2", self)
-        label3 = QLabel("#3", self)
-        label4 = QLabel("#4", self)
-        label5 = QLabel("#5", self)
-        
-        label1.setStyleSheet("background-color: red;")
-        label2.setStyleSheet("background-color: yellow;")
-        label3.setStyleSheet("background-color: green;")
-        label4.setStyleSheet("background-color: blue;")
-        label5.setStyleSheet("background-color: purple;")
-        
-        vbox = QVBoxLayout()
-        vbox.addWidget(label1)
-        vbox.addWidget(label2)
-        vbox.addWidget(label3)
-        vbox.addWidget(label4)
-        vbox.addWidget(label5)
-        central_widget.setLayout(vbox)
     def controls(self):
-        pass
+       lTitle=QLabel("TimeFlow", self)
+       lTitle.setFont(QFont("Arial", 30))
+       lTitle.setGeometry(0,0,500,100)
+       lTitle.setAlignment(Qt.AlignCenter)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
